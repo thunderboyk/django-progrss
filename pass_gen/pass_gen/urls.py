8f893360d109746b8b8434d2354d0b21a47c6209
thunderboyk <lakshyajun27@gmail.com>
2024-09-05 23:53:13 +0530

    Add files via upload

--- pass_gen/pass_gen/urls.py
+++ pass_gen/pass_gen/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
+from django.urls import path
+from generator import views
+
+# here i just made a veiws path for my custom website 
+# give some name go for views.___name will be here
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   path('',),
 ]
+
