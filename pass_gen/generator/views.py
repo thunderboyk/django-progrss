8287d22cedcee31db50dc0f863d4571b026a8caa
thunderboyk <lakshyajun27@gmail.com>
2024-09-06 22:55:22 +0530

    Add files via upload
    some one comes to the website and goes to the homepage and in the url file there is an input of it and sees there is a path for the home page we just took input from and then it goes to aur home.html and gives out the dictionary just we created and and display it :) sorry for bad language

--- pass_gen/generator/views.py
+++ pass_gen/generator/views.py
@@ -5,7 +5,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def yes(request):
-	return render(request, 'generator/home.html')
+	return render(request, 'generator/home.html', {'password': 'amfnevnwm'})
+
+# so basically the password code is for making a dictonary and we can now pass a key in the home html
+# and put keys and get password as per say 
 
 def eggs(request):
 	return HttpResponse('<h1>eggs godd ye s approves<h1>')	
